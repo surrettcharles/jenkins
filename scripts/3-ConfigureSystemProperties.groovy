b78c7a8dd5c1264cf894d6fa4eeee0aceb430c23
surrettcharles <surrettcharles@gmail.com>
2018-05-13 15:58:19 -0500

    added missing ";", addressed potential null for labels

--- scripts/3-ConfigureSystemProperties.groovy
+++ scripts/3-ConfigureSystemProperties.groovy
@@ -30,53 +30,53 @@ Yaml yaml = new Yaml();
 String configPath = System.getenv("CONFIG_PATH");
 String configText = ''
 try {
-    configText = new File("${configPath}/main_config.yml").text
+    configText = new File("${configPath}/main_config.yml").text;
 } catch (FileNotFoundException e) {
-    logger.severe("Cannot find config file path @ ${configPath}/main_config.yml")
-    jenkins.doSafeExit(null)
-    System.exit(1)
+    logger.severe("Cannot find config file path @ ${configPath}/main_config.yml");
+    jenkins.doSafeExit(null);
+    System.exit(1);
 }
 
 def config = yaml.load(configText);
-Map mainConfig = config.MAIN
-Map propertiesConfig = config.GLOBAL_PROPERTIES
-Map locationConfig = config.LOCATION
-Map shellConfig = config.SHELL
+Map mainConfig = config.MAIN;
+Map propertiesConfig = config.GLOBAL_PROPERTIES;
+Map locationConfig = config.LOCATION;
+Map shellConfig = config.SHELL;
 
-logger.info("Configuring basic Jenkins options")
+logger.info("Configuring basic Jenkins options");
 try {
-    jenkins.setSystemMessage(mainConfig.SYSTEM_MESSAGE)
-    jenkins.setNumExecutors(mainConfig.NUMBER_OF_EXECUTORS)
-    jenkins.setLabelString(mainConfig.LABELS.join(' '))
-    jenkins.setQuietPeriod(mainConfig.QUIET_PERIOD)
-    jenkins.setScmCheckoutRetryCount(mainConfig.SCM_RETRY_COUNT)
-    jenkins.setDisableRememberMe(mainConfig.DISABLE_REMEMBER_ME)
+    jenkins.setSystemMessage(mainConfig.SYSTEM_MESSAGE);
+    jenkins.setNumExecutors(mainConfig.NUMBER_OF_EXECUTORS);
+    jenkins.setLabelString((mainConfig.LABELS == null ? '' : mainConfig.LABELS.join(' ')));
+    jenkins.setQuietPeriod(mainConfig.QUIET_PERIOD);
+    jenkins.setScmCheckoutRetryCount(mainConfig.SCM_RETRY_COUNT);
+    jenkins.setDisableRememberMe(mainConfig.DISABLE_REMEMBER_ME);
     if (mainConfig.USAGE == 'NORMAL') {
-        jenkins.setMode(Mode.NORMAL)
+        jenkins.setMode(Mode.NORMAL);
     } else if (mainConfig.USAGE == 'EXCLUSIVE') {
-        jenkins.setMode(Mode.EXCLUSIVE)
+        jenkins.setMode(Mode.EXCLUSIVE);
     }
     else {
-        logger.severe('Invalid value specified for USAGE')
-        logger.severe('Exiting')
-        jenkins.doSafeExit(null)
-        System.exit(1)
+        logger.severe('Invalid value specified for USAGE');
+        logger.severe('Exiting');
+        jenkins.doSafeExit(null);
+        System.exit(1);
     }
 } catch (MissingMethodException e) {
-    logger.severe("Invalid value specified for MAIN configuration in ${configPath}/main_config.yml")
-    jenkins.doSafeExit(null)
-    System.exit(1)
+    logger.severe("Invalid value specified for MAIN configuration in ${configPath}/main_config.yml");
+    jenkins.doSafeExit(null);
+    System.exit(1);
 }
 
-logger.info("Adding global environment variables to Jenkins")
-List<Entry> envVarList = new ArrayList<Entry>()
+logger.info("Adding global environment variables to Jenkins");
+List<Entry> envVarList = new ArrayList<Entry>();
 propertiesConfig.ENVIRONMENT_VARIABLES.each { envVar ->
                 try {
-                    envVarList.add(new Entry(envVar.NAME, envVar.VALUE))
+                    envVarList.add(new Entry(envVar.NAME, envVar.VALUE));
                 } catch (MissingMethodException e) {
-                    logger.severe("Invalid value specified for environment variables in ${configPath}/main_config.yml")
-                    jenkins.doSafeExit(null)
-                    System.exit(1)
+                    logger.severe("Invalid value specified for environment variables in ${configPath}/main_config.yml");
+                    jenkins.doSafeExit(null);
+                    System.exit(1);
                 }
 }
 
@@ -84,30 +84,30 @@ jenkins.getGlobalNodeProperties().replaceBy(
     Collections.singleton(
         new EnvironmentVariablesNodeProperty(envVarList)
     )
-)
+);
 
-logger.info("Setting up the Jenkins URL")
-JenkinsLocationConfiguration location = jenkins.getExtensionList(jenkins.model.JenkinsLocationConfiguration).get(0)
+logger.info("Setting up the Jenkins URL");
+JenkinsLocationConfiguration location = jenkins.getExtensionList(jenkins.model.JenkinsLocationConfiguration).get(0);
 try {
-    location.setUrl(locationConfig.URL)
-    location.setAdminAddress(locationConfig.ADMIN_EMAIL)
+    location.setUrl(locationConfig.URL);
+    location.setAdminAddress(locationConfig.ADMIN_EMAIL);
 } catch (MissingMethodException e) {
-    logger.severe("Invalid value in the LOCATION configuration of ${configPath}/main_config.yml")
-    jenkins.doSafeExit(null)
-    System.exit(1)
+    logger.severe("Invalid value in the LOCATION configuration of ${configPath}/main_config.yml");
+    jenkins.doSafeExit(null);
+    System.exit(1);
 }
 
-logger.info("Setting the default shell used by Jenkins")
-Process p = "stat ${shellConfig.EXECUTABLE}".execute()
-p.waitForOrKill(1000)
+logger.info("Setting the default shell used by Jenkins");
+Process p = "stat ${shellConfig.EXECUTABLE}".execute();
+p.waitForOrKill(1000);
 if (p.exitcode != 0) {
-    logger.severe("Executable ${shellConfig.EXECUTABLE} not present on system")
-    jenkins.doSafeExit(null)
-    System.exit(1)
+    logger.severe("Executable ${shellConfig.EXECUTABLE} not present on system");
+    jenkins.doSafeExit(null);
+    System.exit(1);
 }
-Shell.DescriptorImpl shell = jenkins.getExtensionList(Shell.DescriptorImpl.class).get(0)
-shell.setShell(shellConfig.EXECUTABLE)
-shell.save()
+Shell.DescriptorImpl shell = jenkins.getExtensionList(Shell.DescriptorImpl.class).get(0);
+shell.setShell(shellConfig.EXECUTABLE);
+shell.save();
 
-jenkins.save()
-logger.info("Finished configuring the main Jenkins options.")
+jenkins.save();
+logger.info("Finished configuring the main Jenkins options.");
